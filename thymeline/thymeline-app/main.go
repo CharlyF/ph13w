021ca4654e2d76d7a7521d6b39dd0a2586847e40
Peter Ashwell <peterwashwell@gmail.com>
2018-11-04 13:57:28 -0500

    [goapp] add 3 new routes

--- thymeline/thymeline-app/main.go
+++ thymeline/thymeline-app/main.go
@@ -1,4 +1,35 @@
-package thymeline_app
+package main
 
 
+import (
+"fmt"
+"net/http"
 
+"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/uploadImage", func(w http.ResponseWriter, r *http.Request) {
+		//vars := mux.Vars(r)
+
+		fmt.Fprintf(w, "{'status': 'ok', 'action': 'uploadImage'}");
+	})
+
+	r.HandleFunc("/createImage", func(w http.ResponseWriter, r *http.Request) {
+		//vars := mux.Vars(r)
+
+		fmt.Fprintf(w, "{'status': 'ok', 'action': 'createImage'}");
+	})
+
+	r.HandleFunc("/listImages", func(w http.ResponseWriter, r *http.Request) {
+		//vars := mux.Vars(r)
+
+		fmt.Fprintf(w, "{'status': 'ok', 'action': 'listImages'}");
+	})
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		fmt.Errorf(err.Error());
+	}
+}
